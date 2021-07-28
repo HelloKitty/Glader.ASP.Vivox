430c74b4def6b4e2e5d4cb4be5ef63a4525f0f4d
HelloKitty <andrew.blakely@ymail.com>
2021-07-28 12:06:24 -0500

    Added doc to VivoxChannelJoinResponse

--- src/Glader.ASP.Vivox.Models/Models/Responses/VivoxChannelJoinResponse.cs
+++ src/Glader.ASP.Vivox.Models/Models/Responses/VivoxChannelJoinResponse.cs
@@ -5,6 +5,9 @@ using Newtonsoft.Json;
 
 namespace Glader.ASP.Vivox
 {
+	/// <summary>
+	/// The response model for successfully joining a Vivox channel.
+	/// </summary>
 	[JsonObject]
 	public record VivoxChannelJoinResponse(string AuthToken, string ChannelUri);
 }
