9045786d35cf5c2ec517fd30c0c7a749dc2ce289
HelloKitty <andrew.blakely@ymail.com>
2021-07-28 16:11:30 -0500

    Added documentation to ICharacterSessionInfo

--- src/Glader.ASP.Vivox.Server/Models/ICharacterSessionInfo.cs
+++ src/Glader.ASP.Vivox.Server/Models/ICharacterSessionInfo.cs
@@ -4,10 +4,19 @@ using System.Text;
 
 namespace Glader.ASP.Vivox
 {
+	/// <summary>
+	/// Data provider for the character's session information.
+	/// </summary>
 	public interface ICharacterSessionInfo
 	{
+		/// <summary>
+		/// The id of the map the character is within.
+		/// </summary>
 		int MapId { get; set; }
 
+		/// <summary>
+		/// The optional instance id of the map the character is within.
+		/// </summary>
 		int InstanceId { get; set; }
 	}
 }
