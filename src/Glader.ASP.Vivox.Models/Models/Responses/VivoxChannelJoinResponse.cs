564d2b23776a0d891fd4e9b17b9d165b6eb8df68
HelloKitty <andrew.blakely@ymail.com>
2021-07-28 11:38:58 -0500

    Added VivoxChannelJoinResponse from GladMMO

---
+++ src/Glader.ASP.Vivox.Models/Models/Responses/VivoxChannelJoinResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Glader.ASP.Vivox
+{
+	[JsonObject]
+	public record VivoxChannelJoinResponse(string AuthToken, string ChannelUri);
+}
