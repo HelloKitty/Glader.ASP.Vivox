480bd0bc1610205d7f293a37303f2a3b4fcc4622
HelloKitty <andrew.blakely@ymail.com>
2021-07-29 15:32:01 -0500

    Added IVivoxAPIKeyRepository

---
+++ src/Glader.ASP.Vivox.Server/Services/Database/IVivoxAPIKeyRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Glader.ASP.Vivox
+{
+	/// <summary>
+	/// Contract for providers that provide an API key for Vivox.
+	/// </summary>
+	public interface IVivoxAPIKeyRepository
+	{
+		/// <summary>
+		/// Retrieves the Vivox API key from the store.
+		/// </summary>
+		/// <param name="token">The cancel token.</param>
+		/// <returns>API key.</returns>
+		Task<string> RetrieveKey(CancellationToken token = default);
+	}
+}
