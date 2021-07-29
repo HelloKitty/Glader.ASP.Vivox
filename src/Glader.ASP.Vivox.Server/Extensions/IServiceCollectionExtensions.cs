eb0a256f1cf3cb1a169c7f90ddf319eb0eb7ff08
HelloKitty <andrew.blakely@ymail.com>
2021-07-29 16:27:27 -0500

    Added Vivox token service registeration extension

--- src/Glader.ASP.Vivox.Server/Extensions/IServiceCollectionExtensions.cs
+++ src/Glader.ASP.Vivox.Server/Extensions/IServiceCollectionExtensions.cs
@@ -18,7 +18,38 @@ namespace Glader.ASP.Vivox
 		public static IServiceCollection RegisterVivoxCharacterDataRepository<TDataRepositoryType>(this IServiceCollection services) 
 			where TDataRepositoryType : class, ICharactersDataRepository
 		{
+			if (services == null) throw new ArgumentNullException(nameof(services));
 			return services.AddTransient<ICharactersDataRepository, TDataRepositoryType>();
+		}
+
+		/// <summary>
+		/// Registers a <see cref="IVivoxTokenSignService"/> and <see cref="IVivoxAPIKeyRepository"/>
+		/// in the provided <see cref="services"/>.
+		/// </summary>
+		/// <param name="services"></param>
+		/// <returns></returns>
+		public static IServiceCollection RegisterVivoxTokenServices<TVivoxSignServiceType, TVivoxAPIKeyRepositoryType>(this IServiceCollection services)
+			where TVivoxSignServiceType : class, IVivoxTokenSignService
+			where TVivoxAPIKeyRepositoryType : class, IVivoxAPIKeyRepository
+		{
+			if (services == null) throw new ArgumentNullException(nameof(services));
+			return services.AddTransient<IVivoxTokenSignService, TVivoxSignServiceType>()
+				.AddTransient<IVivoxAPIKeyRepository, TVivoxAPIKeyRepositoryType>();
+		}
+
+		/// <summary>
+		/// Registers a <see cref="IVivoxTokenSignService"/> and <see cref="IVivoxAPIKeyRepository"/>
+		/// in the provided <see cref="services"/>.
+		/// Using <see cref="DefaultLocalVivoxTokenSigningService"/> for <see cref="IVivoxTokenSignService"/>.
+		/// </summary>
+		/// <param name="services"></param>
+		/// <returns></returns>
+		public static IServiceCollection RegisterVivoxTokenServices<TVivoxAPIKeyRepositoryType>(this IServiceCollection services)
+			where TVivoxAPIKeyRepositoryType : class, IVivoxAPIKeyRepository
+		{
+			if(services == null) throw new ArgumentNullException(nameof(services));
+			return services.AddTransient<IVivoxTokenSignService, DefaultLocalVivoxTokenSigningService>()
+				.AddTransient<IVivoxAPIKeyRepository, TVivoxAPIKeyRepositoryType>();
 		}
 	}
 }
