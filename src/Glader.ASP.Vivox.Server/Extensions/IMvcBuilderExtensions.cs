13a03cf7d1aa9beb01a0fe4ec2e507b1d6810514
HelloKitty <andrew.blakely@ymail.com>
2021-07-28 22:29:22 -0500

    Added Extensions

---
+++ src/Glader.ASP.Vivox.Server/Extensions/IMvcBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Glader.ASP.GameConfig;
+using Glader.Essentials;
+using GladMMO;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Glader.ASP.Vivox
+{
+	public static class IMvcBuilderExtensions
+	{
+		/// <summary>
+		/// Registers the <see cref="VivoxAccountController"/> with the MVC
+		/// controllers. See controller documentation for what it does and how it works.
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static IMvcBuilder RegisterVivoxControllers(this IMvcBuilder builder)
+		{
+			if (builder == null) throw new ArgumentNullException(nameof(builder));
+			return builder.RegisterController<VivoxAccountController>()
+				.RegisterController<VivoxChannelController>();
+		}
+	}
+}
---
+++ src/Glader.ASP.Vivox.Server/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.ASP.Vivox;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Glader.ASP.Vivox
+{
+	public static class IServiceCollectionExtensions
+	{
+		/// <summary>
+		/// Registers a <see cref="ICharactersDataRepository"/>
+		/// in the provided <see cref="services"/>.
+		/// </summary>
+		/// <param name="services">Service container.</param>
+		/// <returns></returns>
+		public static IServiceCollection RegisterVivoxCharacterDataRepository<TDataRepositoryType>(this IServiceCollection services) 
+			where TDataRepositoryType : class, ICharactersDataRepository
+		{
+			return services.AddTransient<ICharactersDataRepository, TDataRepositoryType>();
+		}
+	}
+}
