aea02fc7c6022bf9a6c0753c89949ff7069775a8
HelloKitty <andrew.blakely@ymail.com>
2024-02-17 02:47:32 -0600

    Create IVivoxCredentialsProvider.cs

---
+++ src/Glader.ASP.Vivox.Server/Services/IVivoxCredentialsProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.ASP.Vivox
+{
+	public interface IVivoxCredentialsProvider
+	{
+		string VivoxIssuer { get; }
+
+		string VivoxDomain { get; }
+	}
+}
