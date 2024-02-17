d396b644b3e7436d5219e66d77d2dea3b66daf75
HelloKitty <andrew.blakely@ymail.com>
2024-02-17 02:49:03 -0600

    Create StaticVivoxCredentialsProvider.cs

---
+++ src/Glader.ASP.Vivox.Server/Services/StaticVivoxCredentialsProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.ASP.Vivox
+{
+	public sealed class StaticVivoxCredentialsProvider : IVivoxCredentialsProvider
+	{
+		public string VivoxIssuer { get; }
+
+		public string VivoxDomain { get; }
+
+		public StaticVivoxCredentialsProvider(string vivoxIssuer, string vivoxDomain)
+		{
+			if (string.IsNullOrWhiteSpace(vivoxIssuer)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(vivoxIssuer));
+			if (string.IsNullOrWhiteSpace(vivoxDomain)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(vivoxDomain));
+
+			VivoxIssuer = vivoxIssuer;
+			VivoxDomain = vivoxDomain;
+		}
+	}
+}
