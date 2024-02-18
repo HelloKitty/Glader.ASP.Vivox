97d4aa52a07c3a2beb4f853b4533be26c321b525
HelloKitty <andrew.blakely@ymail.com>
2024-02-18 02:32:22 -0600

    Added CanJoinProximityChatAsync to the Vivox Client

--- src/Glader.ASP.Vivox.Client/IVivoxAuthorizationService.cs
+++ src/Glader.ASP.Vivox.Client/IVivoxAuthorizationService.cs
@@ -20,5 +20,15 @@ namespace Glader.ASP.Vivox
 		[RequiresAuthentication]
 		[Post("/api/VivoxChannel/proximity/join")]
 		Task<ResponseModel<VivoxChannelJoinResponse, VivoxLoginResponseCode>> JoinProximityChatAsync(CancellationToken token = default);
+
+		/// <summary>
+		/// Call this before calling <see cref="JoinProximityChatAsync"/> to check if you're able to join the proximity voice chat channel yet.
+		/// </summary>
+		/// <param name="mapId">The map id to check joining availability.</param>
+		/// <param name="token">Cancel token.</param>
+		/// <returns>True if the provided <see cref="mapId"/> is available to join via <see cref="JoinProximityChatAsync"/>.</returns>
+		[RequiresAuthentication]
+		[Get("/api/VivoxChannel/proximity/{mapId}/available")]
+		Task<bool> CanJoinProximityChatAsync([AliasAs("mapId")] int mapId, CancellationToken token = default);
 	}
 }
